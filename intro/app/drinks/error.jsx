632a533f67660935f2f188e5f33a7966d3de1281
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-12-09 12:16:11 -0500

    tested next loading and error file functionality

---
+++ intro/app/drinks/error.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+const error = (error) => {
+  console.log(error);
+  return (
+    <div>there was an error . . . </div>
+  )
+}
+export default error;
--- intro/app/drinks/page.jsx
+++ intro/app/drinks/page.jsx
@@ -1,9 +1,22 @@
+
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a';
+
+const fetchDrinks = async () => {
+  // await new Promise(resolve => setTimeout(resolve, 1000));
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch drinks');
+  }
+
+  const data = await response.json();
+  return data;
+}
+
 const DrinksPage = async () => {
 
-  const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a';
+  const data = await fetchDrinks();
 
-  const response = await fetch(url);
-  const data = await response.json();
   console.log(Object.keys(data));
 
   return (
