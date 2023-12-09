a4b24d4c1f31f3f7ab89c2693a11e7df87830059
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-12-09 11:40:31 -0500

    used fetch api in a server component/no use effect

--- intro/app/drinks/page.jsx
+++ intro/app/drinks/page.jsx
@@ -1,6 +1,15 @@
-const DrinksPage = () => {
+const DrinksPage = async () => {
+
+  const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a';
+
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(Object.keys(data));
+
   return (
-    <div>Drinks Page</div>
+    <div>
+      <h1 className="text-4xl">Drinks Page</h1>
+    </div>
   )
 }
 export default DrinksPage;
--- intro/app/page.jsx
+++ intro/app/page.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 
 const HomePage = () => {
+
   return (
     <div className="">
       <h1 className="text-5xl mb-8 font-bold">Next.js Tutorial</h1>
