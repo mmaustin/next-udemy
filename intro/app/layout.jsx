e4440c1779556603798cbfbb9d2933a64c16df08
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-12-09 10:38:17 -0500

    imported navbar to main layout component

--- intro/app/layout.jsx
+++ intro/app/layout.jsx
@@ -1,5 +1,7 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Navbar from '@/components/Navbar';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,8 +14,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <nav className="text-2xl text-primary">navbar</nav>
-        {children}
+        <Navbar />
+        <main className="px-8 py-20 max-w-6xl mx-auto">
+          {children}
+        </main>
       </body>
     </html>
   )
--- intro/components/Navbar.jsx
+++ intro/components/Navbar.jsx
@@ -10,10 +10,21 @@ const links = [
 const Navbar = () => {
   return (
     <nav className="bg-base-300 py-4">
-      <div className="navbar px-8 max-w-6xl max-auto flex-col sm:flex-row">
+      <div className="navbar px-8 max-w-6xl mx-auto flex-col sm:flex-row">
         <Link href='/' className="btn btn-primary">
           Next.js
         </Link>
+        <ul className="menu menu-horizontal md:ml-8">
+          {links.map(link => {
+            return (
+              <li key={link.href}>
+                <Link href={link.href} className="capitalize">
+                  {link.label}
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
       </div>
     </nav>
   )
