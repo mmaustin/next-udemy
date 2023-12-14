bef0ffa226db8975483402a243bd49aa6a083228
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-12-14 11:06:58 -0500

    used next image to set up static image rendering

--- intro/app/drinks/[id]/page.jsx
+++ intro/app/drinks/[id]/page.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
@@ -22,6 +23,7 @@ const SingleDrinkPage = async ({ params }) => {
       <Link href='/drinks' className="btn btn-primary mt-8 mb-12">
         back to drinks
       </Link>
+      <Image src={imgSrc} className="w-48 h-48 rounded-lg" alt='drink' />
       <h1 className="text-4xl mb-8">{title}</h1>
     </div>
   )
