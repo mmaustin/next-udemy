288a66e27e8d3f4a90a1997d0500002f594f98b4
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-12-18 11:41:12 -0500

    displayed tasks in task list component

--- intro/components/TaskList.jsx
+++ intro/components/TaskList.jsx
@@ -1,8 +1,29 @@
+import prisma from "@/utils/db";
 
+const TaskList = async () => {
 
-const TaskList = () => {
+  const allTasks = await prisma.task.findMany({
+    orderBy: {
+      createAt: 'desc',
+    },
+  });
+
+  if (allTasks.length === 0) {
+    return (
+      <h2 className="mt-8 font-medium text-lg">No tasks to show . . . </h2>
+    )
+  }
+
   return (
-    <div>TaskList</div>
+    <ul className="mt-8">
+      {allTasks.map(task => {
+        return <li key={task.id} className="flex justify-between items-center px-6 py-4 mb-4 border border-base-300 rounded-lf shadow-lg">
+          <h2 className={`text-lg capitalize ${task.completed ? 'line-through' : null}`}>
+            {task.content}
+          </h2>
+        </li>
+      })}
+    </ul>
   )
 }
 export default TaskList;
